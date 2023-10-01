740b3ab652259867901ad188cdde0d1febf6842d
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-09-30 20:44:41 -0400

    wrote basic HTML for testing Homepage and components

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,4 +1,6 @@
 // This is the Shared Layout of CampusCart
 export default function Layout() {
-
+    return (<>
+        <h1>This is the Layout</h1>
+    </>)
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,10 @@
 // This is the Homepage of CampusCart
+import Layout from "@/components/layout";
+export default function Homepage() {
+    return (
+        <div>
+            <Layout />
+            <h1>This is the Homepage</h1>
+        </div>
+    )
+}
