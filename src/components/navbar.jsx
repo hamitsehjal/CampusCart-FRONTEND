6e3e4c17efec6c3ba74d212fec7766a0b7170b7e
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-03 04:31:19 -0400

    Configure logic for mobile toggle

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,28 +1,53 @@
 // This is the Navbar Component of CampusCart
-import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import { ShoppingCartIcon, Bars4Icon, XCircleIcon } from '@heroicons/react/24/outline';
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <div className='mx-auto'>
 
-            <nav className="flex  flex-row justify-around ">
+            <nav className="flex  flex-row justify-between md:px-4 px-2 ">
                 {/* Logo  */}
-                <div className="basis-2/5 flex flex-row gap-x-2 items-center justify-start text-2xl">
-                    <ShoppingCartIcon className="h-16 w-16 text-red-700" />
-                    <span className="text-3xl">CampusCart</span>
+                <div className="flex flex-row gap-x-2 items-center justify-start text-2xl">
+                    <ShoppingCartIcon className="h-6  md:h-10 lg:h-16 w-6 md:w-10 lg:w-16 text-red-700" />
+                    <span className="text-xl md:text-2xl">CampusCart</span>
                 </div>
 
                 {/* Home 
             Contact US  */}
-                <div className="basis-1/4 flex flex-row gap-x-4 items-center justify-start text-xl">
+                <div className="hidden lg:flex flex-row gap-x-4 items-center justify-start text-xl">
                     <a href='#'>Home</a>
+                    <a href='#'>Benefits</a>
                     <a href='#'>Contact Us</a>
                 </div>
 
                 {/* Log In 
             Sign Up  */}
-                <div className="basis-1/4 flex flex-row gap-x-4 items-center justify-end text-xl">
+                <div className="hidden lg:flex flex-row gap-x-4 items-center justify-end text-xl">
                     <a href='#'>Log In</a>
                     <a href='#'>Sign Up</a>
+                </div>
+                {/* hamburger Icon for Mobile navigation */}
+                <div className='lg:hidden flex flex-col items-center justify-center text-xl'>
+                    <button onClick={toggleMenu}>
+                        {!isOpen ?
+                            <Bars4Icon className='h-6 w-6' /> : <XCircleIcon className='h-6 w-6' />
+                        }
+
+                    </button>
+
+                    {isOpen && <div className='flex flex-col items-center'>
+                        <a href='#'>Home</a>
+                        <a href='#'>Benefits</a>
+                        <a href='#'>Contact Us</a>
+                        <a href='#'>Log In</a>
+                        <a href='#'>Sign Up</a>
+                    </div>}
                 </div>
 
             </nav>
