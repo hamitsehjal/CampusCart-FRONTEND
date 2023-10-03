b2c74ec69d583c572072eda5b67fb208469a4e81
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-03 05:13:17 -0400

    extend custom colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     './src/**/*.{jsx,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary Brand colors 
+        'campus-red': '#8D0801',
+        'campus-blue': '#282A3E ',
+
+        // Secondary Brand colors 
+        'campus-secondary': '#66748F',
+
+        // Additional colors 
+        'campus-background': '#E6EDEF',
+        'campus-text': '#0B090A',
+        'campus-accent': '#B50603',
+      }
+    },
   },
   plugins: [],
 }
