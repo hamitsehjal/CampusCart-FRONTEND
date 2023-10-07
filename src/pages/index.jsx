fe8425ee12bf74e7d966242946deb6b3fabe2f5f
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-07 02:40:12 -0400

    Responsive Footer Up and Running✔️

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,9 @@
 export default function Homepage() {
     return (
         <div>
-            <h1>CampusCart</h1>
+            <h1 className="text-center">Home page Content goes here....</h1>
+            <h1>&nbsp;</h1>
+            <h1>&nbsp;</h1>
         </div>
     )
 }
