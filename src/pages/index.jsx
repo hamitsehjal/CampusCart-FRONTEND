e27f622a90216ea9828d1193f3c817fd5ef04c3e
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-07 01:42:53 -0400

    Configure Layout to use append content of each page between Navbar and Footer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 // This is the Homepage of CampusCart
 export default function Homepage() {
     return (
-        <>
-        </>
+        <div>
+            <h1>CampusCart</h1>
+        </div>
     )
 }
