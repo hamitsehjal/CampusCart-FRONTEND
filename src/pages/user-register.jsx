60f39a1d209fb57668c4cc84804e911856b0abe3
Dariazrn <dzyrianova@myseneca.ca>
2023-10-09 00:18:02 -0400

    Added remove profile picture button

--- src/pages/user-register.jsx
+++ src/pages/user-register.jsx
@@ -1,8 +1,5 @@
 // This is the User-Registration of CampusCart
-
-// This is the User-Registration of CampusCart
 import { useState } from "react";
-
 export default function UserRegister() {
   const [formData, setFormData] = useState({
     fullName: "",
@@ -11,12 +8,18 @@ export default function UserRegister() {
     password: "",
     profilePicture: null,
   });
-
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     setFormData({
       ...formData,
       profilePicture: file,
+    });
+  };
+
+  const removeProfilePicture = () => {
+    setFormData({
+      ...formData,
+      profilePicture: null,
     });
   };
 
@@ -26,24 +29,38 @@ export default function UserRegister() {
         <h1 className="text-lg text-campus-text font-cinzel mb-6 text-center">
           Student Registration
         </h1>
-
         <div className="mb-4">
           <label className="block text-campus-text font-medium font-noto_serif mb-2">
             Profile Picture
           </label>
+
+          {formData.profilePicture && (
+            <div className="mt-5 text-center">
+              {/*Display Profile Picture*/}
+              <div className="flex justify-center items-center">
+                <img
+                  src={URL.createObjectURL(formData.profilePicture)}
+                  alt="Profile Preview"
+                  className="h-40 w-40 object-cover rounded-full "
+                />
+              </div>
+              {/*Remove Button*/}
+              <button
+                onClick={removeProfilePicture}
+                className="text-sm font-noto_serif text-white bg-campus-red hover:bg-campus-accent py-1 px-2 mt-2 mb-2 rounded-md cursor-pointer"
+              >
+                Remove
+              </button>
+            </div>
+          )}
+          {/*Upload Profile Picture*/}
           <input
             type="file"
             name="profilePicture"
             accept="image/*"
             onChange={handleImageChange}
+            className="mb-2"
           />
-          {formData.profilePicture && (
-            <img
-              src={URL.createObjectURL(formData.profilePicture)}
-              alt="Profile Preview"
-              className="mt-5 h-40 w-40 object-cover rounded-full"
-            />
-          )}
         </div>
       </div>
     </div>
