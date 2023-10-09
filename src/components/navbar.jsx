1a08dd5a95817bee62ef21a51c0a581662b2c1ed
Dariazrn <dzyrianova@myseneca.ca>
2023-10-09 00:12:13 -0400

    Add a profile picture functionality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,70 +1,100 @@
 // This is the Navbar Component of CampusCart
-import { useState } from 'react';
-import { RiShoppingBasket2Line, RiMenuLine, RiCloseCircleLine, RiLoginBoxLine, RiBodyScanLine } from 'react-icons/ri'
-import Link from 'next/link';
+import { useState } from "react";
+import {
+  RiShoppingBasket2Line,
+  RiMenuLine,
+  RiCloseCircleLine,
+  RiLoginBoxLine,
+  RiBodyScanLine,
+} from "react-icons/ri";
+import Link from "next/link";
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    }
-    return (
-        <nav className='bg-campus-red flex flex-col'>
-            <div className="flex flex-row justify-between p-4 md:p-2  text-campus-background">
-                {/* Logo  */}
-                <div>
-                    <Link className="flex flex-row gap-x-2 items-center justify-start text-2xl" href='#'>
-                        <RiShoppingBasket2Line className="h-10  md:h-10 lg:h-16 w-10 md:w-10 lg:w-16 text-campus-background" />
-                        <span className="text-lg md:text-2xl font-noto_serif">CampusCart</span>
-                    </Link>
-                </div>
-                {/* Home 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav className="bg-campus-red flex flex-col">
+      <div className="flex flex-row justify-between p-4 md:p-2  text-campus-background">
+        {/* Logo  */}
+        <div>
+          <Link
+            className="flex flex-row gap-x-2 items-center justify-start text-2xl"
+            href="#"
+          >
+            <RiShoppingBasket2Line className="h-10  md:h-10 lg:h-16 w-10 md:w-10 lg:w-16 text-campus-background" />
+            <span className="text-lg md:text-2xl font-noto_serif">
+              CampusCart
+            </span>
+          </Link>
+        </div>
+        {/* Home 
             Contact US  */}
-                <div className="hidden lg:flex flex-row gap-x-4 items-center justify-start text-xl">
-                    <Link href='/' className='font-noto_serif'>Home</Link>
-                    <Link href='/stores' className='font-noto_serif'>Stores</Link>
-                    <Link href='#' className='font-noto_serif'>Contact Us</Link>
-                </div>
+        <div className="hidden lg:flex flex-row gap-x-4 items-center justify-start text-xl">
+          <Link href="/" className="font-noto_serif">
+            Home
+          </Link>
+          <Link href="/stores" className="font-noto_serif">
+            Stores
+          </Link>
+          <Link href="#" className="font-noto_serif">
+            Contact Us
+          </Link>
+        </div>
 
-                {/* Log In 
+        {/* Log In 
             Sign Up  */}
-                <div className="hidden lg:flex flex-row gap-x-4 items-center justify-end text-xl">
-                    <Link className='px-4 py-2 font-noto_serif text-sm bg-campus-text text-campus-background rounded-3xl hover:bg-campus-background hover:text-campus-text' href='#'>Login</Link>
-                    <Link className='px-4 py-2 bg-campus-text font-noto_serif text-sm text-campus-background rounded-3xl hover:bg-campus-background hover:text-campus-text' href='#'>Sign Up</Link>
-                </div>
-                {/* Mobile navigation */}
-                <div className='lg:hidden flex flex-col items-center justify-center text-xl'>
-                    <button onClick={toggleMenu}>
-                        {!isOpen ?
-                            <RiMenuLine className='h-6 w-6' /> : <RiCloseCircleLine className='h-6 w-6' />
-                        }
+        <div className="hidden lg:flex flex-row gap-x-4 items-center justify-end text-xl">
+          <Link
+            className="px-4 py-2 font-noto_serif text-sm bg-campus-text text-campus-background rounded-3xl hover:bg-campus-background hover:text-campus-text"
+            href="#"
+          >
+            Login
+          </Link>
+          <Link
+            className="px-4 py-2 bg-campus-text font-noto_serif text-sm text-campus-background rounded-3xl hover:bg-campus-background hover:text-campus-text"
+            href="/user-register"
+          >
+            Sign Up
+          </Link>
+        </div>
+        {/* Mobile navigation */}
+        <div className="lg:hidden flex flex-col items-center justify-center text-xl">
+          <button onClick={toggleMenu}>
+            {!isOpen ? (
+              <RiMenuLine className="h-6 w-6" />
+            ) : (
+              <RiCloseCircleLine className="h-6 w-6" />
+            )}
+          </button>
+        </div>
+      </div>
 
-                    </button>
-                </div>
-
-            </div>
-
-            {isOpen && <div className='grid grid-cols-2 lg:hidden gap-2 p-4 text-campus-background font-noto_serif'>
-                {/* Links */}
-                <div className='flex flex-col'>
-                    <Link href='/'>Home</Link>
-                    <Link href='/stores'>Stores</Link>
-                    <Link href='#'>Contact Us</Link>
-                </div>
-                {/* Buttons */}
-                <div className='flex flex-col gap-2 justify-end p-2'>
-                    <Link className="flex flex-row gap-2 items-center" href='#'>
-                        <RiLoginBoxLine className="h-6 w-6" />
-                        <span className='text-sm'>Login</span>
-                    </Link>
-                    <Link className='flex flex-row gap-2 items-center' href='#'>
-                        <RiBodyScanLine className='h-6 w-6' />
-                        <span className='text-sm'>Sign Up</span>
-                    </Link>
-                </div>
-            </div>}
-        </nav>
-
-    )
+      {isOpen && (
+        <div className="grid grid-cols-2 lg:hidden gap-2 p-4 text-campus-background font-noto_serif">
+          {/* Links */}
+          <div className="flex flex-col">
+            <Link href="/">Home</Link>
+            <Link href="/stores">Stores</Link>
+            <Link href="#">Contact Us</Link>
+          </div>
+          {/* Buttons */}
+          <div className="flex flex-col gap-2 justify-end p-2">
+            <Link className="flex flex-row gap-2 items-center" href="#">
+              <RiLoginBoxLine className="h-6 w-6" />
+              <span className="text-sm">Login</span>
+            </Link>
+            <Link
+              className="flex flex-row gap-2 items-center"
+              href="/user-register"
+            >
+              <RiBodyScanLine className="h-6 w-6" />
+              <span className="text-sm">Sign Up</span>
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
 }
--- src/pages/user-register.jsx
+++ src/pages/user-register.jsx
@@ -1,1 +1,51 @@
 // This is the User-Registration of CampusCart
+
+// This is the User-Registration of CampusCart
+import { useState } from "react";
+
+export default function UserRegister() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    studentNumber: "",
+    email: "",
+    password: "",
+    profilePicture: null,
+  });
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      profilePicture: file,
+    });
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-campus-background">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
+        <h1 className="text-lg text-campus-text font-cinzel mb-6 text-center">
+          Student Registration
+        </h1>
+
+        <div className="mb-4">
+          <label className="block text-campus-text font-medium font-noto_serif mb-2">
+            Profile Picture
+          </label>
+          <input
+            type="file"
+            name="profilePicture"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+          {formData.profilePicture && (
+            <img
+              src={URL.createObjectURL(formData.profilePicture)}
+              alt="Profile Preview"
+              className="mt-5 h-40 w-40 object-cover rounded-full"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
