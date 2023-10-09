89656e6844999bef92bf66b5254de17ab9d6fafa
bhavik001 <bhmistry@myseneca.ca>
2023-10-09 05:54:29 -0400

    Added code for the HomePage with image

--- public/index.js
+++ public/index.js
@@ -1,21 +1,25 @@
 // This repo holds all the static assets of our application
 
 export const StoreImages = {
-    SevenEleven: "/images/stores/7-Eleven.webp",
-    Costco: "/images/stores/costco.webp",
-    DollarTree: "/images/stores/dollar-tree.webp",
-    FoodsCo: "/images/stores/foodsco.webp",
-    IndianMart: "/images/stores/indian-market.webp",
-    Staples: "/images/stores/staples.webp",
+  SevenEleven: "/images/stores/7-Eleven.webp",
+  Costco: "/images/stores/costco.webp",
+  DollarTree: "/images/stores/dollar-tree.webp",
+  FoodsCo: "/images/stores/foodsco.webp",
+  IndianMart: "/images/stores/indian-market.webp",
+  Staples: "/images/stores/staples.webp",
 };
 
 export const CategoriesImages = {
-    grocery: "/images/categories/grocery.webp",
-    alcohol: "/images/categories/Alcohol.webp",
-    wholesale: "/images/categories/beauty.webp",
-    convenience: "/images/categories/convenience.webp",
-    pets: "/images/categories/pets.webp",
-    beauty: "/images/categories/beauty.webp",
-    pharmacy: "/images/categories/pharmacy.webp",
-    retail: "/images/categories/retail.webp",
-}
+  grocery: "/images/categories/grocery.webp",
+  alcohol: "/images/categories/Alcohol.webp",
+  wholesale: "/images/categories/beauty.webp",
+  convenience: "/images/categories/convenience.webp",
+  pets: "/images/categories/pets.webp",
+  beauty: "/images/categories/beauty.webp",
+  pharmacy: "/images/categories/pharmacy.webp",
+  retail: "/images/categories/retail.webp",
+};
+
+export const HomePageImage = {
+  grocery1: "/images/homePage/grocery-cart.jpg",
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,36 @@
 // This is the Homepage of CampusCart
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { HomePageImage } from '../../public'
+
+
+
 export default function Homepage() {
-    return (
-        <div>
-            <h1 className="text-center">Home page Content goes here....</h1>
-            <h1>&nbsp;</h1>
-            <h1>&nbsp;</h1>
+  return (
+    <div className="outline-none border-none relative">
+      <div className="absolute left-[30px] md:left-[70px] max-w-[250px] sm:max-w-[550px] top-[50%] -translate-y-[50%] space-y-2 lg:space-y-4 bg-[#ffffffa2] sm:bg-transparent p-4 sm:p-0 rounded-lg campus-accent">
+        <h1 className="text-campus-accent text-4xl lg:text-[70px] font-bold leading-[1.2]">
+          Bring the Store to your Doors
+        </h1>
+        <h3 className="text-black font-bold text-[24px] lg:text-[48px] leading-[1.2]">Best Groceries app ever. Pick your desired groceries from the menu.</h3>
+
+        <div className="bg-campus-accent text-white text-[24px] p-2 px-4 rounded-lg inline-block cursor-pointer hover:bg-campus-secondary">
+          Join Now
         </div>
-    )
+      </div>
+
+      <Image
+        className="w-[100%] h-[400px] md:h-auto object-cover object-right md:object-left-bottom"
+        src={HomePageImage.grocery1}
+        alt="banner"
+        width={2000}
+        height={2000}
+      />
+    </div>
+    
+    
+  );
 }
+
+    
