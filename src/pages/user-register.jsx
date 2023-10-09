0921bf5ae874cd5657c0fcd969f3c573575c7516
Dariazrn <dzyrianova@myseneca.ca>
2023-10-09 00:24:59 -0400

    Added Registration Input Fields

--- src/pages/user-register.jsx
+++ src/pages/user-register.jsx
@@ -1,10 +1,12 @@
 // This is the User-Registration of CampusCart
 import { useState } from "react";
+
 export default function UserRegister() {
   const [formData, setFormData] = useState({
-    fullName: "",
-    studentNumber: "",
-    email: "",
+    firstName: "",
+    lastName: "",
+    studentId: "",
+    emailAddress: "",
     password: "",
     profilePicture: null,
   });
@@ -15,14 +17,12 @@ export default function UserRegister() {
       profilePicture: file,
     });
   };
-
   const removeProfilePicture = () => {
     setFormData({
       ...formData,
       profilePicture: null,
     });
   };
-
   return (
     <div className="flex justify-center items-center h-screen bg-campus-background">
       <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
@@ -33,7 +33,6 @@ export default function UserRegister() {
           <label className="block text-campus-text font-medium font-noto_serif mb-2">
             Profile Picture
           </label>
-
           {formData.profilePicture && (
             <div className="mt-5 text-center">
               {/*Display Profile Picture*/}
@@ -60,6 +59,73 @@ export default function UserRegister() {
             accept="image/*"
             onChange={handleImageChange}
             className="mb-2"
+          />
+        </div>
+        {/* Student First Name */}
+        <div className="mb-4">
+          <label className="block text-campus-text font-noto_serif font-medium mb-2">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Kate"
+            className="border border-black rounded-md p-2 w-full"
+            value={formData.firstName}
+          />
+        </div>
+
+        {/* Student Last Name */}
+        <div className="mb-4">
+          <label className="block text-campus-text font-noto_serif font-medium mb-2">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Smith"
+            className="border border-black rounded-md p-2 w-full"
+            value={formData.lastName}
+          />
+        </div>
+        {/* Student Number */}
+        <div className="mb-4">
+          <label className="block text-campus-text font-noto_serif font-medium mb-2">
+            Seneca Student Number
+          </label>
+          <input
+            type="text"
+            name="studentId"
+            placeholder="123456789"
+            className="border border-black rounded-md p-2 w-full"
+            value={formData.studentId}
+          />
+        </div>
+        {/* Student Email */}
+        <div className="mb-4">
+          <label className="block text-campus-text font-noto_serif font-medium mb-2">
+            Email Address
+          </label>
+          <input
+            type="email"
+            name="emailAddress"
+            placeholder="katesmith@myseneca.ca"
+            className="border border-black rounded-md p-2 w-full"
+            value={formData.emailAddress}
+          />
+        </div>
+
+        {/* Student Password */}
+        <div className="mb-4">
+          <label className="block text-campus-text font-noto_serif font-medium mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="*************"
+            className="border border-black rounded-md p-2 w-full"
+            value={formData.password}
           />
         </div>
       </div>
