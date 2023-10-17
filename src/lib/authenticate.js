ddc3b9d3c751a456072fd99a0f5d3941b4dca8a2
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-17 12:30:36 -0400

    Logic for registering User and Issuing tokens

--- src/lib/authenticate.js
+++ src/lib/authenticate.js
@@ -1,6 +1,34 @@
 import jwt_decode from 'jwt-decode';
 
 /**
+ * 1. Given the User information, make a request to our Server's api/register route
+ * 2. If the response's status code is 201, redirect to Login route 
+ * 3. If the status code is not 201, throw an error with our api's error message 
+ * 
+ */
+export async function registerUser(userData) {
+    const res = await fetch(`http://localhost:8080/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...userData }),
+    });
+
+    const data = await res.json();
+
+    if (res.status === 201) {
+        return true;
+    } else {
+        /**
+         * Our Error object from server has this structure:
+         * https://github.com/hamitsehjal/CampusCart-BACKEND/blob/main/src/response.js#L35C7-L35C8
+         */
+        throw new Error(data.error.message);
+    }
+}
+
+/**
  * 1. Given an email and password, it should obtain JWT from our Server's api/login route
  * 2. Store the JWT token locally if status code of above request's response is 200
  * 3. If status code is not 200, throw an error with the error message from the API
