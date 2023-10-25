b8b6260e220e5f945e95842e97076186c4e17b58
Дарья <Dasha@MacBook-Pro-Dara.local>
2023-10-25 01:52:15 -0400

    Cart Page - Checkout Added

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -24,7 +24,7 @@ const Cart = () => {
   return (
    
     <div class="h-screen bg-gray-100 pt-20">
-    <h1 class="text-lg text-campus-text font-cinzel mb-6 text-center">Cart Items</h1>
+    <h1 class="text-2xl text-campus-text font-cinzel mb-6 text-center">Cart Items</h1>
     <div class="mx-auto max-w-screen-lg justify-start md:flex md:space-x-4">
       <div class="rounded-lg md:w-2/3 overflow-y-auto max-h-[70vh]">
       <div class="h-50 w-100 mr-1 shadow-md justify-between mb-6 rounded-lg bg-white p-6  sm:flex sm:justify-start "> 
@@ -36,8 +36,8 @@ const Cart = () => {
         />
         <div class="sm:flex sm:w-full sm:justify-between">
             <div class= "font-noto_serif font-medium">
-              <h2 class="text-lg font-bold">Strawberry</h2>
-              <p class="text-sm">CAD$ 3.10</p>
+              <h2 class="text-lg font-cinzel font-bold">Strawberry 1LB</h2>
+              <p class="text-sm font-noto_serif">CAD$ 3.10</p>
             </div>
 
               <div class="flex items-center border-gray-100">
@@ -81,8 +81,8 @@ const Cart = () => {
         />
         <div class="sm:flex sm:w-full sm:justify-between">
             <div class= "font-noto_serif font-medium">
-              <h2 class="text-lg font-bold">Avacado Bag</h2>
-              <p class="text-sm">CAD$ 2.15</p>
+              <h2 class="text-lg font-cinzel font-bold">Avacado Bag</h2>
+              <p class="text-sm font-noto_serif">CAD$ 2.15</p>
             </div>
 
               <div class="flex items-center border-gray-100">
@@ -125,8 +125,8 @@ const Cart = () => {
         />
         <div class="sm:flex sm:w-full sm:justify-between">
             <div class= "font-noto_serif font-medium">
-              <h2 class="text-lg font-bold">Onion</h2>
-              <p class="text-sm">CAD$ 0.58</p>
+              <h2 class="text-lg font-cinzel font-bold">Onion</h2>
+              <p class="text-sm font-noto_serif">CAD$ 0.58</p>
             </div>
 
               <div class="flex items-center border-gray-100">
@@ -161,6 +161,25 @@ const Cart = () => {
         </div>
       </div>
 
+      <div class="shadow-md mt-0 md:w-1/3 h-full rounded-lg border bg-white p-8">
+        <div class="flex justify-between">
+          <p class="font-noto_serif font-medium text-black">Subtotal</p>
+          <p class="font-noto_serif font-medium text-black">CAD$10.00</p>
+        </div>
+        <div class="flex justify-between">
+          <p class="font-noto_serif font-medium text-black">Tax</p>
+          <p class="font-noto_serif font-medium text-black">CAD$0.13</p>
+        </div>
+        <hr class="my-3" />
+        <div class="flex justify-between">
+          <p class="font-cinzel font-medium text-black text-lg">Total</p>
+          <div class="my-3">
+            <p class="mb-1 text-lg font-cinzel font-bold text-black">CAD$10.13</p>
+            <p class="text-sm font-noto_serif font-medium text-black">including Tax</p>
+          </div>
+        </div>
+        <button class="font-noto_serif font-medium text-white mt-3 w-full rounded-full bg-campus-red py-2 hover:bg-campus-accent">Check out</button>
+      </div>
     </div>
   </div>
   )
