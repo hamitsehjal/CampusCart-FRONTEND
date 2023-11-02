e90c0e586d1f6bf4a1ed8e9cf72e166c553fa09c
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-11-02 02:28:38 -0400

    Add remotePatterns for AWS S3 Images

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3.us-east-1.amazonaws.com',
+            },
+        ],
+    }
+}
