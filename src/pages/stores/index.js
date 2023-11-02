b1f615df723241c1af08468f1708f53b1ae7eb72
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-11-02 00:09:15 -0400

    replace useState,useEffect with useSWR for fetching stores

--- src/pages/stores/index.js
+++ src/pages/stores/index.js
@@ -1,76 +1,59 @@
 // src/pages/stores/index.js
-import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { getToken } from 'lib/authenticate';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 export default function Stores() {
-    const [stores, setStores] = useState([]);
-    const [error, setError] = useState(null);
     const router = useRouter();
 
-    // function to handle the click event 
-    function handleStoreClick(storeId) {
-        router.push(`/products/${storeId}`);
+
+    const fetcher = (...args) => fetch(...args).then(res => res.json());
+    const options = {
+        headers: {
+            'Authorization': `Bearer ${getToken()}`,
+            'Content-Type': 'application/json',
+        }
     }
+    const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_BACKEND_API}/private/stores`, url => fetcher(url, options));
 
-    useEffect(() => {
-        async function fetchStores() {
-            try {
-                const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/private/stores`, {
-                    headers: {
-                        'Authorization': `Bearer ${getToken()}`,
-                        'Content-Type': 'application/json',
-                    }
-                });
-                if (!res.ok) {
-                    console.error(`HTTP Error: Status: ${res.status}`);
-                    setError('Failed to fetch data');
-                    return
-                }
-                const result = await res.json();
-                setStores(result.data);
-            } catch (err) {
-                console.error(`Error Occurred while fetching data: ${err}`);
-                setError(`An Error occurred while fetching data`);
-                return;
-            }
 
-        };
-        fetchStores();
-    }, []);
-    if (!stores) return (<h2>Loading Stores!!</h2>)
     return (
         <div className="bg-campus-background p-4 md:p-8 lg:p-10">
             <h1 className="text-2xl md:text-3xl font-noto_serif ">Stores to help you save</h1>
-            {/* Stores */}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 gap-2 font-noto_serif'>
-                {error ? (
-                    <h2>Error: {error}</h2>
-                ) : (
-                    stores.map((store) => {
-                        return <div
-                            key={store._id}
-                            onClick={(e) => router.push(`/products/${store._id}`)}
-                            className="p-2 w-full  mx-auto bg-campus-background rounded-xl shadow-lg flex  items-center space-x-4" >
-                            <div className="shrink-0">
-                                <img src={store.imageUrl}
-                                    alt={`${store.name} Logo`}
-                                    width={80} height={60}
-                                />
-                            </div>
-                            <div>
-                                <div className="text-xl font-medium text-campus-text  border-black rounded-xl">
-                                    {store.name}
-                                </div>
-                                <div className="flex flex-wrap text-slate-500 text-sm">
-                                    {store.category.join(' ')}
-                                </div>
-                            </div>
-                        </div>
-                    }))
-                }
+            {/* Store Categories */}
+            {/* <div className='flex flex-wrap space-x-2 font-noto_serif'>
 
-            </div>
+            </div> */}
+            {/* Stores */}
+            {error ? (<h2>Error Occurred</h2>
+            ) : isLoading ? (<h2>Loading Data</h2>
+            ) : (
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 gap-2 font-noto_serif'>
+                    {
+                        data.map((store) => {
+                            return <div
+                                key={store._id}
+                                onClick={(e) => router.push(`/products/${store._id}`)}
+                                className="p-2 w-full  mx-auto bg-campus-background rounded-xl shadow-lg flex  items-center space-x-4" >
+                                <div className="shrink-0">
+                                    <Image src={store.imageUrl}
+                                        alt={`${store.name} Logo`}
+                                        width={80} height={60}
+                                    />
+                                </div>
+                                <div>
+                                    <div className="text-xl font-medium text-campus-text  border-black rounded-xl">
+                                        {store.name}
+                                    </div>
+                                    <div className="flex flex-wrap text-slate-500 text-sm">
+                                        {store.category.join(' ')}
+                                    </div>
+                                </div>
+                            </div>
+                        })
+                    }
+
+                </div>)}
         </div >)
 
 }
