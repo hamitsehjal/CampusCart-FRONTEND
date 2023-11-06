ce87c908a2158fa50071b30e5338fc36e3d9418c
Replit user <>
2023-11-06 01:01:15 +0000

    Configure hook to fetch products for a store from Backend Server

---
+++ src/utils/useProducts.js
@@ -0,0 +1,18 @@
+/** 
+* Hook for fetching Products from Backend
+* 1. Receives storeId as parameters
+* 2. Make SWR request to URL: "/private/products/:storedId"
+* 3. Return Object containing data, error, and isLoading values
+*/
+
+import useSWR from 'swr';
+
+export default function useProducts(id = storeId, options) {
+  const { data, error, isLoading } = useSWR([`${process.env.NEXT_PUBLIC_BACKEND_API}/private/products/:${id}`, options]);
+
+  return {
+    productsData: data,
+    productsError: error,
+    productsLoading: isLoading,
+  }
+}
