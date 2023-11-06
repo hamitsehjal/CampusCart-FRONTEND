52c48c031d73b10f629477aa67df7a83cdf5bee4
Replit user <>
2023-11-06 03:06:59 +0000

    Refactor the URL for including category

--- src/utils/useProducts.js
+++ src/utils/useProducts.js
@@ -1,14 +1,14 @@
 /** 
 * Hook for fetching Products from Backend
 * 1. Receives storeId as parameters
-* 2. Make SWR request to URL: "/private/products/:storedId"
+* 2. Make SWR request to URL: "/private/products/:storedId?category=${category}
 * 3. Return Object containing data, error, and isLoading values
 */
 
 import useSWR from 'swr';
 
-export default function useProducts(id = storeId, options = {}) {
-  const { data, error, isLoading } = useSWR([`${process.env.NEXT_PUBLIC_BACKEND_API}/private/products/:${id}`, options]);
+export default function useProducts(id, category = "all", options = {}) {
+  const { data, error, isLoading } = useSWR([`${process.env.NEXT_PUBLIC_BACKEND_API}/private/products/:${id}?category=${category}`, options]);
 
   return {
     productsData: data,
