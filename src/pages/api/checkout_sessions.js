fe4ec74bb506fa5ba4a6184d01b803b13a7fbd2f
Replit user <>
2023-11-15 00:52:29 +0000

    Configure Stripe Checkout Session

--- src/pages/api/checkout_sessions.js
+++ src/pages/api/checkout_sessions.js
@@ -1,15 +1,13 @@
 const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
-import { useSelector } from 'react-redux';
-import { selectCartItems } from '../../store/cartSelector';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       // Get latest items from the Redux Store 'cart' state
-      const cartItems = useSelector(selectCartItems);
-      console.log(cartItems);
+      const cartItems = req.body;
 
+      console.log("Cart Items:", cartItems);
 
       /** 
       Create Stripe Products from Cart Items
@@ -18,7 +16,7 @@ export default async function handler(req, res) {
       - function to create a singe product
       - returns product id  
       */
-      const products = cartItems.map(async (item) => {
+      const products = await Promise.all(cartItems.map(async (item) => {
         const product = await stripe.products.create({
           name: item.name,
           images: [item.image],
@@ -26,9 +24,10 @@ export default async function handler(req, res) {
         return {
           id: product.id,
           quantity: item.quantity,
-          price: (item.price * 1000),
+          price: (item.price * 100),
         };
-      })
+      }));
+      console.log("Products: ", products);
       /** 
       Create Stripe Prices from Products
       Check out official Stripe Docs: https://stripe.com/docs/api/prices/create
@@ -37,7 +36,7 @@ export default async function handler(req, res) {
       - returns product id  
       */
 
-      const lineItems = products.map(async (item) => {
+      const lineItems = await Promise.all(products.map(async (item) => {
         const data = await stripe.prices.create({
           product: item.id,
           unit_amount: item.price,
@@ -47,18 +46,21 @@ export default async function handler(req, res) {
           price: data.id,
           quantity: item.quantity,
         }
-      });
+      }));
 
       console.log(lineItems);
 
       // Create checkout Session
-      await stripe.checkout.sessions.create({
+
+      const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
-        success_url: `{req.headers.origin}?success=true`,
-        cancel_url: `{req.headers.origin}?canceled=true`,
+        success_url: `${process.env.NEXT_PUBLIC_DOMAIN}/order-confirmation`,
+        cancel_url: `${process.env.NEXT_PUBLIC_DOMAIN}/cart`,
       })
-      res.redirect(303, sesson.url);
+      res.json({ url: session.url });
+
+
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
