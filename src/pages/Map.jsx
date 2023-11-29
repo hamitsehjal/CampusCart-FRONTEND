79bab1b75032382eb02733fb08d9630b3f37c73f
bhavik001 <bhmistry@myseneca.ca>
2023-11-29 03:45:19 -0500

    Adding pick-up location selection

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const Map = () => {
   const mapContainerStyle = {
-    width: '80%',
-    height: '400px',
+    width: '100%',
+    height: '100%',
     borderRadius: '20px'
   };
 
@@ -15,71 +15,145 @@ const Map = () => {
   ];
 
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [currentPosition, setCurrentPosition] = useState(null);
 
-  const handleLocationClick = (index) => {
-    const location = locations[index];
-    setSelectedLocation(location);
+  useEffect(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCurrentPosition({ lat: latitude, lng: longitude });
+      },
+      (error) => {
+        console.error('Error getting current location:', error);
+      }
+    );
+  } else {
+    alert('Geolocation is not supported by your browser');
+  }
+}, []);
+
+  const calculateDistance = (coord1, coord2) => {
+    const R = 6371; // Earth radius in kilometers
+    const dLat = (coord2.lat - coord1.lat) * (Math.PI / 180);
+    const dLon = (coord2.lng - coord1.lng) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(coord1.lat * (Math.PI / 180)) * Math.cos(coord2.lat * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance.toFixed(2); // Round to 2 decimal places
   };
 
-  const handleShowAllClick = () => {
-    setSelectedLocation(null);
+  const [selectedSpot, setSelectedSpot] = useState(null);
+  const [isLocationConfirmed, setIsLocationConfirmed] = useState(false);
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState(null);
+
+  const handleSelectSpot = (index) => {
+    const location = locations[index];
+    setSelectedSpot(location);
+    setIsLocationConfirmed(false); // Reset confirmation when selecting a new spot
+  };
+
+  const handleSelectLocation = () => {
+    setIsLocationConfirmed(true);
+  };
+  const handleConfirmLocation = () => {
+    setIsLocationConfirmed(true);
+    window.location.href = "/cart";
+  };
+
+  const resetButtonStyles = () => {
+    setSelectedSpot(null);
+    setSelectedButtonIndex(null);
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "20px",
-        paddingBottom: "20px",
-      }}
-    >
-      <h1 className='font-bold pt-4 text-2xl font-serif'>Select The Location</h1>
-      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-        <GoogleMap
-          mapContainerStyle={mapContainerStyle}
-          zoom={selectedLocation ? 15 : 10}
-          center={selectedLocation ? selectedLocation.position : { lat: 43.850033105714054, lng: -79.36710996197064 }}
-        >
-          {locations.map((location, index) => (
-            <MarkerF
-              key={index}
-              position={location.position}
-              onClick={() => handleLocationClick(index)}
-            />
-          ))}
-        </GoogleMap>
-      </LoadScript>
-
-      <div className="grid grid-rows-4 grid-flow-col gap-4">
-        {locations.map((location, index) => (
-          <button
-            key={index}
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'
-            onClick={() => handleLocationClick(index)}
+    <div className="flex h-screen">
+      <div className="w-2/3 h-full relative">
+        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+          <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={selectedLocation ? 15 : 10}
+            center={selectedLocation ? selectedLocation.position : (currentPosition || { lat: 43.850033105714054, lng: -79.36710996197064 })}
           >
-            {location.name}
-          </button>
-        ))}
-        
-        {/* Button to show all locations */}
-        <button
-          className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-lg'
-          onClick={handleShowAllClick}
-        >
-          Show All
-        </button>
+            {locations.map((location, index) => (
+              <Marker
+                key={index}
+                position={location.position}
+                onClick={() => setSelectedLocation(location)}
+              />
+            ))}
+            {currentPosition && (
+              <Marker
+                position={currentPosition}
+                icon={{
+                  path: window.google.maps.SymbolPath.CIRCLE,
+                  scale: 8,
+                  fillColor: '#4285F4',
+                  fillOpacity: 1,
+                  strokeWeight: 1,
+                }}
+              />
+            )}
+          </GoogleMap>
+        </LoadScript>
       </div>
+      <div className='w-1/3 h-full p-4 bg-gray-100'>
+        <div className="grid grid-rows-6 grid-flow-col gap-4">
+          {locations.map((location, index) => (
+            <button
+              key={index}
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg ${selectedSpot === location ? 'border-2 border-yellow-500' : ''} ${selectedButtonIndex === index ? 'bg-green-500' : ''}`}
+              onClick={() => {
+                handleSelectSpot(index);
+                setSelectedLocation(location);
+                setSelectedButtonIndex(index);
+              }}
+            >
+              {location.name} - {currentPosition && calculateDistance(currentPosition, location.position)} km
+            </button>
+          ))}
 
-      {selectedLocation && (
-        <div>
-          <div style={{ backgroundColor: 'Red', padding: '10px', borderRadius: '8px' }}>
-            <p className='text-white font-bold'>{selectedLocation.address}</p>
-          </div>
+          {/* Button to show all locations */}
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'
+            onClick={() => {
+              setSelectedLocation(null);
+              setSelectedSpot(null);
+              resetButtonStyles(); // Reset styles for all buttons
+            }}
+          >
+            Show All
+          </button>
+
+          {/* Select Spot Button */}
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg ${selectedSpot ? '' : 'cursor-not-allowed'}`}
+            onClick={handleSelectLocation}
+            disabled={!selectedSpot || isLocationConfirmed}
+          >
+            Select Spot
+          </button>
+
+          {/* Confirm Location Button */}
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg ${isLocationConfirmed ? '' : 'cursor-not-allowed'}`}
+            onClick={handleConfirmLocation}
+            disabled={!isLocationConfirmed}
+          >
+            Confirm Location
+          </button>
         </div>
-      )}
+
+        {selectedSpot && (
+          <div className='py-2'>
+            <div style={{ backgroundColor: 'Red', padding: '10px', borderRadius: '8px' }}>
+              <p className='text-white font-bold'>{selectedSpot.address}</p>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
