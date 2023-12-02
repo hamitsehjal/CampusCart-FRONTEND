73e25fb8fdceac7fec455bf2ebf562cbf03b7c2c
Дарья <Dasha@MacBook-Pro-Dara.local>
2023-12-02 06:09:36 -0500

    Dashboard-v1.1

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,10 @@
 // Dashboard.jsx
 import React, { useState } from "react";
 import { useRouter } from 'next/router';
-
+import Image from "next/image";
 const Dashboard = (props) => {
+
+  const categories = ["Fruits", "Vegetables", "Electronics",];
 
   
   // DUMMY PRODUCTS DATA
@@ -16,19 +18,47 @@ const Dashboard = (props) => {
 
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isUpdateModalOpen, setUpdateModalOpen] = useState(false);
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
 
+  const clearFormData = { 
+  };
+  const [formData, setFormData] = useState(clearFormData);
 
-  // OPEN MODAL
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setFormData({
+      ...formData,
+      profile: file,
+    });
+  };
+  const removeProfile = () => {
+    setFormData({
+      ...formData,
+      profile: null,
+    });
+  };
+
+  // OPEN UPDATE MODAL
   const openUpdateModal = (product) => {
     setSelectedProduct(product);
     setUpdateModalOpen(true);
   };
 
-  // CLOSE MODAL
+  // CLOSE UPDATE MODAL
   const closeUpdateModal = () => {
     setSelectedProduct(null);
     setUpdateModalOpen(false);
   };
+
+// OPEN ADD MODAL
+  const openAddModal = () => {
+    setAddModalOpen(true);
+    };
+    
+// CLOSE ADD MODAL
+  const closeAddModal = () => {
+    setAddModalOpen(false);
+    };
 
   // TODO: DELETE PRODUCT
   const handleDelete = (productId) => {
@@ -59,11 +89,101 @@ const Dashboard = (props) => {
         </div>
         {/* ADD A NEW PRODUCT BUTTON */}
         <div className="mr-4">
-          <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">
-            Add a new product
-          </button>
+        <button
+          onClick={openAddModal}
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+        >
+          Add a new product
+        </button>
         </div>
       </div>
+      {isAddModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
+        <div className="bg-white p-8 rounded-md flex relative" style={{ maxHeight: "80vh", overflowY: "auto" }}>
+        <button
+            onClick={closeAddModal}
+            className="text-2xl absolute top-2 right-2 text-gray-600 cursor-pointer"
+        >
+        &times;
+        </button>
+        {/* ADD MODAL FORM */}
+        <form className="flex flex-col items-center">
+        {formData.profile && (
+            <div className="mt-5 text-center">
+              {/*Display Profile Picture*/}
+              <div className="flex justify-center items-center">
+                <Image
+                  src={URL.createObjectURL(formData.profile)}
+                  alt="Profile Preview"
+                  className="object-cover "
+                  width={200}
+                  height={200}
+                />
+              </div>
+              {/*Remove Button*/}
+              <button
+                onClick={removeProfile}
+                className="text-sm font-noto_serif text-white bg-campus-red hover:bg-campus-accent py-1 px-2 mt-2 mb-2 rounded-md cursor-pointer"
+              >
+                Remove
+              </button>
+            </div>
+          )}
+          {/*Upload Profile Picture*/}
+          <input
+            type="file"
+            name="profile"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="mb-2"
+          />
+
+              <div>
+                <label className="block text-sm text-campus-text font-medium">Product Name:</label>
+                <input
+                  type="text"
+                  className="mt-1 p-2 border rounded"
+                />
+              </div>
+              <div className="mt-4">
+                <label className="block text-sm text-campus-text font-medium">Category:</label>
+                <select
+            className="mt-1 p-2 border rounded"
+          >
+            {categories.map((category) => (
+              <option key={category} value={category.toLowerCase()}>
+                {category}
+              </option>
+            ))}
+          </select>
+              </div>
+              <div className="mt-4">
+                <label className="block text-sm text-campus-text font-medium">Price:</label>
+                <input
+                  type="number"
+                  step="0.01"
+                  className="mt-1 p-2 border rounded"
+                />
+              </div>
+              <div className="mt-4">
+                <label className="block text-sm text-campus-text font-medium">Description:</label>
+                <textarea
+                  className="mt-1 p-2 border rounded"
+                />
+              </div>
+              <button
+                type="button"
+                className="mt-4 bg-campus-blue text-white p-2 rounded hover:bg-campus-blue-accent"
+                onClick={() => {
+                    closeAddModal();
+                  }}
+              >
+                Add Product
+              </button>
+            </form>
+        </div>
+        </div>
+    )}
 
       {/* PRODUCTS LIST (DUMMY FOR NOW) */}
       <div className="grid grid-cols-1 gap-4">
