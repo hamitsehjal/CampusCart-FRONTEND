c248bbea163224636451478d79dffeaf4db379ad
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-12-03 21:57:40 -0500

    Add Data Filtering

--- src/pages/dashboard/DashboardHeader.js
+++ src/pages/dashboard/DashboardHeader.js
@@ -1,11 +1,36 @@
 import React from "react";
-
+import { useState } from 'react';
+import useProductCategories from 'utils/useProductCategories';
+import Category from '@/components/category';
+import { getToken } from "lib/authenticate";
 //SEARCH AND ADD A NEW PRODUCT
 const DashboardHeader = ({ openAddModal }) => {
+
+  // Configure Options object for private Access
+  const options = {
+    headers: {
+      'Authorization': `Bearer ${getToken('store')}`,
+      'Content-Type': `application/json`,
+    }
+  }
+
+  const [category, setCategory] = useState('all');
+  // Extract the product Categories Details
+  const { productCategoriesData, productCategoriesError, productCategoriesLoading } = useProductCategories(options);
+
+  if (productCategoriesLoading) {
+    // Render loading state
+    return <p>Loading...</p>;
+  }
+
+  if (productCategoriesError) {
+    // Render error state
+    return <p>Error: {productCategoriesError.message}</p>;
+  }
   return (
     <div className="flex justify-between items-center mb-8">
       <div className="flex items-center justify-center">
-        <input
+        {/* <input
           type="text"
           placeholder="Search..."
           className="p-3 border rounded w-90 mr-4 ml-28"
@@ -13,7 +38,15 @@ const DashboardHeader = ({ openAddModal }) => {
         />
         <button className="bg-campus-red text-white p-3 rounded hover:bg-campus-accent mr-10">
           Search
-        </button>
+        </button> */}
+        {/* Products Categories */}
+        <Category
+          category={category}
+          setCategory={setCategory}
+          data={productCategoriesData}
+          error={productCategoriesError}
+          isLoading={productCategoriesLoading}
+        />
       </div>
       <div className="mr-4">
         <button
